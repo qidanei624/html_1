f53dd728a3cb4b68b970ead75f2c1bf8ee3f795e
lh <qidanei624@163.com>
2015-05-18 20:52:42 +0800

    sdf

--- caipiao624/application/views/shama/menu_tab.php
+++
@@ -1,8 +0,0 @@
-<ul class="typecho-option-tabs">
-	<li><a href="javascript:void(0)">杀 码：</a></li>
-	<li<?php if('shama_a' == $status): ?> class="current"<?php endif; ?>><?php echo anchor('admin/counteract4/manage/shama_a','杀码_A');?></li>
-	<li<?php if('shama_b' == $status): ?> class="current"<?php endif; ?>><?php echo anchor('admin/counteract4/manage/shama_b','杀码_B');?></li>
-	<li<?php if('shama_c' == $status): ?> class="current"<?php endif; ?>><?php echo anchor('admin/counteract4/manage/shama_c','杀码_C');?></li>
-	<li<?php if('sk_a' == $status): ?> class="current"<?php endif; ?>><?php echo anchor('admin/counteract4/manage/sk_a','生克_A');?></li>
-	
-</ul>
--- caipiao624/application/views/shama/menu_tab3.php
+++
@@ -1,6 +0,0 @@
-<ul class="typecho-option-tabs">
-	<li><a href="javascript:void(0)">胆 码：</a></li>
-	<li<?php if('shama_a' == $status): ?> class="current"<?php endif; ?>><?php echo anchor('admin/counteract4/manage/shama_a','?');?></li>
-	
-	
-</ul>
